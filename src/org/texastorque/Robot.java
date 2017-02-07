951d1004e9c76d8c5ccf1a504ee287828a577c09
Glen Lauritsen <DanielHurin@gmail.com>
2017-02-07 07:19:42 -0600

    basic methods added to the Robot class

--- src/org/texastorque/Robot.java
+++ src/org/texastorque/Robot.java
@@ -3,45 +3,35 @@ package org.texastorque;
 import org.texastorque.torquelib.base.TorqueIterative;
 
 public class Robot extends TorqueIterative {
-	
+
 	@Override
 	public void robotInit() {
-		
+
 	}
 
 	@Override
 	public void autonomousInit() {
-		
+
 	}
-	
+
 	@Override
 	public void teleopInit() {
-		
+
 	}
-	
-	@Override
-	public void autonomousPeriodic() {
-		
-	}
-	
-	@Override
-	public void teleopPeriodic() {
-		
-	}
-	
+
 	@Override
 	public void autonomousContinuous() {
-		
+
 	}
-	
+
 	@Override
 	public void teleopContinuous() {
-		
+
 	}
-	
+
 	@Override
 	public void alwaysContinuous() {
-		
+
 	}
-	
+
 }
