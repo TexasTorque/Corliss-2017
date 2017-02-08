eb3e1beba624cfc7552db5044593aaed4f7d9fa1
Glen Lauritsen <DanielHurin@gmail.com>
2017-02-08 07:18:48 -0600

    initialized the Ports class for DriveBase and Shooter

--- src/org/texastorque/constants/Ports.java
+++ src/org/texastorque/constants/Ports.java
@@ -1,5 +1,14 @@
 package org.texastorque.constants;
 
 public class Ports {
+	
+//	drivebase ports
+	public static final int DB_LEFTFORE = -1;
+	public static final int DB_LEFTREAR = -1;
+	public static final int DB_RIGHTFORE = -1;
+	public static final int DB_RIGHTREAR = -1;
+//	shooter ports
+	public static final int FW_LEFT = -1;
+	public static final int FW_RIGHT = -1;
 
 }
