7029890c44babece5174b5b83955adc6ee83b69c
Glen Lauritsen <DanielHurin@gmail.com>
2017-02-09 10:42:07 -0600

    updated Constants with all values needed for FlyWheel system

--- src/org/texastorque/constants/Constants.java
+++ src/org/texastorque/constants/Constants.java
@@ -1,5 +1,13 @@
 package org.texastorque.constants;
 
+import org.texastorque.torquelib.util.Parameters.Constant;
+
 public class Constants {
 
+	public static Constant HI_DBDT = new Constant("HI_DEADBANDDT", 0.1);
+	public static Constant HI_DOBOTHSHOOTERS = new Constant("HI_DOBOTHSHOOTERS", 0.0);
+	
+	public static Constant FW_SS = new Constant("FW_SMALLSHIFT",.01);
+	public static Constant FW_LS = new Constant("FW_LARGESHIFT",.1);
+	
 }
