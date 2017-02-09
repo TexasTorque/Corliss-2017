050862ad153e8738e206bb512f63a10e90fa6159
Glen Lauritsen <DanielHurin@gmail.com>
2017-02-09 10:49:28 -0600

    modified the DriveBase and Subsystem classes

---
+++ src/org/texastorque/subsystem/Subsystem.java
@@ -0,0 +1,10 @@
+package org.texastorque.subsystem;
+
+import org.texastorque.interfaces.TorqueSubsystem;
+import org.texastorque.io.Input;
+
+public abstract class Subsystem implements TorqueSubsystem {
+
+	protected Input i;
+	
+}
