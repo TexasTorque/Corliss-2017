08d8fc25540a8d9ed2d83e24976e7afe35e4d11c
Glen Lauritsen <DanielHurin@gmail.com>
2017-02-09 14:53:26 -0600

    finalized Robot class for basicrobot

--- src/org/texastorque/Robot.java
+++ src/org/texastorque/Robot.java
@@ -1,37 +1,72 @@
 package org.texastorque;
 
+import java.util.ArrayList;
+
+import org.texastorque.io.HumanInput;
+import org.texastorque.io.Input;
+import org.texastorque.io.RobotOutput;
+import org.texastorque.subsystem.Climber;
+import org.texastorque.subsystem.Conveyor;
+import org.texastorque.subsystem.DriveBase;
+import org.texastorque.subsystem.FlyWheel;
+import org.texastorque.subsystem.Intake;
+import org.texastorque.subsystem.Subsystem;
+import org.texastorque.subsystem.Twinsters;
 import org.texastorque.torquelib.base.TorqueIterative;
 
 public class Robot extends TorqueIterative {
 
+	private ArrayList<Subsystem> subsystems;
+	
+	@SuppressWarnings("serial")
 	@Override
 	public void robotInit() {
-
+		Input.getInstance();
+		HumanInput.getInstance();
+		RobotOutput.getInstance();
+		subsystems = new ArrayList<Subsystem>(){{
+			add(Climber.getInstance());
+			add(Conveyor.getInstance());
+			add(DriveBase.getInstance());
+			add(FlyWheel.getInstance());
+			add(Intake.getInstance());
+			add(Twinsters.getInstance());
+		}};
 	}
 
 	@Override
 	public void autonomousInit() {
-
+		for(Subsystem system : subsystems ) {
+			system.autoInit();
+		}
 	}
 
 	@Override
 	public void teleopInit() {
-
+		for(Subsystem system : subsystems ) {
+			system.teleopInit();
+		}
 	}
 
 	@Override
 	public void autonomousContinuous() {
-
+		for(Subsystem system : subsystems ) {
+			system.autoContinuous();
+		}
 	}
 
 	@Override
 	public void teleopContinuous() {
-
+		for(Subsystem system : subsystems ) {
+			system.teleopContinuous();
+		}
 	}
 
 	@Override
 	public void alwaysContinuous() {
-
+		for(Subsystem system : subsystems) {
+			system.smartDashboard();
+		}
 	}
 
 }
