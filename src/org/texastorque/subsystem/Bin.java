2bd5c872bb30716150409268788917c5d697a44f
Glen Lauritsen <DanielHurin@gmail.com>
2017-02-10 16:47:19 -0600

    boilerplate for Bin completed

---
+++ src/org/texastorque/subsystem/Bin.java
@@ -0,0 +1,47 @@
+package org.texastorque.subsystem;
+
+public class Bin extends Subsystem {
+
+	private static Bin instance;
+	
+	@Override
+	public void autoInit() {
+		init();
+	}
+
+	@Override
+	public void teleopInit() {
+		init();
+	}
+
+	private void init() {
+		
+	}
+	
+	@Override
+	public void autoContinuous() {
+		run();
+	}
+
+	@Override
+	public void teleopContinuous() {
+		run();
+	}
+
+	private void run() {
+		output();
+	}
+	
+	private void output() {
+		
+	}
+	
+	@Override
+	public void smartDashboard() {
+	}
+	
+	public static Bin getInstance() {
+		return instance == null ? instance = new Bin() : instance;
+	}
+	
+}
