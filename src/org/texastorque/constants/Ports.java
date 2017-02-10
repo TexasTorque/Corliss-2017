8190e5e1940b7c4a54f1fa6a510ee1476b70f089
Jonathon <Jonathon>
2017-02-10 17:30:03 -0600

    added necessary Gear constants

--- src/org/texastorque/constants/Ports.java
+++ src/org/texastorque/constants/Ports.java
@@ -1,26 +1,29 @@
 package org.texastorque.constants;
 
 public class Ports {
-	
-//	drivebase ports
+
+	// drivebase ports
 	public static final int DB_LEFTFORE = -1;
 	public static final int DB_LEFTREAR = -1;
 	public static final int DB_RIGHTFORE = -1;
 	public static final int DB_RIGHTREAR = -1;
-//	shooter ports
+	// shooter ports
 	public static final int FW_LEFT = -1;
 	public static final int FW_RIGHT = -1;
-//	intake ports
+	// intake ports
 	public static final int IN_UPPER = -1;
 	public static final int IN_LOWER = -1;
-//	conveyor ports
+	// conveyor ports
 	public static final int CN_LEFT = -1;
 	public static final int CN_RIGHT = -1;
-//	twinsters ports
+	// twinsters ports
 	public static final int TW_LEFT = -1;
 	public static final int TW_RIGHT = -1;
-//	climber ports
+	// climber ports
 	public static final int CL_LEFT = -1;
 	public static final int CL_RIGHT = -1;
-
+	// gear ports
+	public static final int GH_SOLE = -1;
+	public static final int GR_LEFT = -1;
+	public static final int GR_RIGHT = -1;
 }
