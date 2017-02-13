18d88562f1e52e5c168d3ab5cefb7dfc1f2bf7b1
Glen Lauritsen <DanielHurin@gmail.com>
2017-02-13 12:35:31 -0600

    began working on boilerplate for Feedback

--- src/org/texastorque/feedback/Feedback.java
+++ src/org/texastorque/feedback/Feedback.java
@@ -2,4 +2,18 @@ package org.texastorque.feedback;
 
 public class Feedback {
 
+	private static Feedback instance;
+	
+	public Feedback() {
+		init();
+	}
+	
+	private void init() {
+		
+	}
+	
+	public static Feedback getInstance() {
+		return instance == null ? instance = new Feedback() : instance;
+	}
+	
 }
