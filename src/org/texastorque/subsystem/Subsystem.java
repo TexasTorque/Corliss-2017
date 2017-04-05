545cb50af427d87f95b11747d66b11af2dafd666
GlenLauritsen <eagleglen2014@gmail.com>
2017-04-05 10:24:13 -0500

    changed the Subsystem master class to reduce clutter in Subsystems

--- src/org/texastorque/subsystem/Subsystem.java
+++ src/org/texastorque/subsystem/Subsystem.java
@@ -1,11 +1,15 @@
 package org.texastorque.subsystem;
 
+import org.texastorque.feedback.Feedback;
 import org.texastorque.interfaces.TorqueSubsystem;
 import org.texastorque.io.Input;
+import org.texastorque.io.RobotOutput;
 
 public abstract class Subsystem implements TorqueSubsystem {
 
-	protected Input i;
+	protected Input i = Input.getInstance();
+	protected RobotOutput o = RobotOutput.getInstance();
+	protected Feedback f = Feedback.getInstance();
 	
 	public void setInput(Input i) {
 		this.i = i;
